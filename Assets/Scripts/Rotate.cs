e17029329535c07ecd0b4510c55cd2fba24f07c5
Roberzs <zhy18125@163.com>
2023-02-26 15:05:52 +0800

    fix bugs

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var eulerAngles = transform.eulerAngles;
+        eulerAngles.y += speed * Time.deltaTime;
+        transform.eulerAngles = eulerAngles;
+    }
+}
